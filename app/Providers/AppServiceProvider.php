1d746026756f13adee414d8d22bfc8e2575b64c0
rnytk <ronytakatik@gmail.com>
2025-06-26 07:06:53 -0600

    config app servide provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,9 +18,9 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+     public function boot(): void
     {
-        if (env('APP_ENV')=== 'production'){
+        if (app()->environment('production')) {
             URL::forceScheme('https');
         }
     }
